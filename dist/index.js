f7827fec12f53c230011e74503dac2afa1d8a8e5
DavidDexter <dmwangi@kineticengines.co.ke>
2020-02-23 08:52:39 +0300

    chore : debug sdkmanager - 2

--- dist/index.js
+++ dist/index.js
@@ -1361,11 +1361,12 @@ function setupAndroid(version) {
         core.exportVariable('ADB_INSTALL_TIMEOUT', '120');
         yield exec.exec('bash -c "sudo mkdir -p $ANDROID_HOME"');
         yield exec.exec(`bash -c "curl --silent --show-error --location --fail --retry 3 --output ${tempDirectory}/$SDK_VERSION https://dl.google.com/android/repository/$SDK_VERSION"`);
-        yield exec.exec(`bash -c "sudo unzip ${tempDirectory}/$SDK_VERSION -d $ANDROID_HOME && sudo rm -rf ${tempDirectory}/$SDK_VERSION "`);
+        yield exec.exec(`bash -c "sudo unzip -q ${tempDirectory}/$SDK_VERSION -d $ANDROID_HOME && sudo rm -rf ${tempDirectory}/$SDK_VERSION "`);
         core.addPath('$ANDROID_HOME/emulator');
         core.addPath('$ANDROID_HOME/tools');
         core.addPath('$ANDROID_HOME/tools/bin');
         core.addPath('$ANDROID_HOME/platform-tools');
+        yield exec.exec(`echo $PATH`);
         console.log('=== installing android SDK ===');
         // await exec.exec(`bash -c "sudo mkdir ${tempDirectory}/.android && sudo echo '### User Sources for Android SDK Manager' | sudo tee -a ${tempDirectory}/.android/repositories.cfg"`)
         yield exec.exec(`sdkmanager --list`);
